10ec3d34500035134a7dd32b3f0d1145244f039c
lylya <gitlylya@mail.ru>
2019-04-27 14:22:59 +0300

    Lesson 1 corrections after 2-nd checking

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,32 +1,30 @@
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int length = size();
 
     void clear() {
-        int length = size();
         for (int i = 0; i < length; i++) {
             storage[i] = null;
         }
+        length = 0;
     }
 
     void save(Resume r) {
         boolean isExists = false;
-        int length = size();
         for (int i = 0; i < length; i++) {
             if (storage[i].uuid == r.uuid) {
-                storage[i] = r;
                 isExists = true;
-                System.out.println("r.uuid уже существует. Элемент обновлен");
                 break;
             }
         }
         if (isExists == false) {
             storage[length] = r;
+            length = length + 1;
         }
     }
 
     Resume get(String uuid) {
         Resume resume = null;
-        int length = size();
         for (int i = 0; i < length; i++) {
             if (storage[i].uuid == uuid) {
                 resume = storage[i];
@@ -38,12 +36,11 @@ public class ArrayStorage {
 
 
     void delete(String uuid) {
-        int counter = size();
-        for (int i = 0; i < counter; i++) {
+        for (int i = 0; i < length; i++) {
             if (storage[i].uuid == uuid) {
-                storage[i] = null;
-                storage[i] = storage[counter - 1];
-                storage[counter - 1] = null;
+                storage[i] = storage[length - 1];
+                storage[length - 1] = null;
+                length = length - 1;
                 break;
             }
         }
@@ -53,7 +50,6 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int length = size();
         Resume[] smallStorage = new Resume[length];
         for (int i = 0; i < length; i++) {
             smallStorage[i] = storage[i];
